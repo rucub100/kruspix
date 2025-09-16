389bbd63e98b31bb2cbe4f677c2b492e3c3e0e96
Ruslan Curbanov <info@ruslan-curbanov.de>
2025-09-16 23:06:11 +0200

    feat(fdt): add FdtReserveEntry struct for reserved memory representation

---
+++ src/drivers/devicetree/fdt_reserve_entry.rs
@@ -0,0 +1,18 @@
+/// Representation of a reserved memory entry in a Flattened Device Tree (FDT).
+#[repr(C), align(8)]
+pub struct FdtReserveEntry {
+    address: u64,
+    size: u64,
+}
+
+impl FdtReserveEntry {
+    /// Returns the starting physical address of the reserved memory region.
+    pub fn address(&self) -> u64 {
+        u64::from_be(self.address)
+    }
+
+    /// Returns the size of the reserved memory region in bytes.
+    pub fn size(&self) -> u64 {
+        u64::from_be(self.size)
+    }
+}
