b57257d0105aab9817f3f809d2086cccefe74937
Ruslan Curbanov <info@ruslan-curbanov.de>
2025-09-18 21:59:37 +0200

    feat(build): add build script and linker script for AArch64 target to support GDB

---
+++ build.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let profile = std::env::var("PROFILE").unwrap();
+    let target = std::env::var("TARGET").unwrap();
+
+    let arch = SupportedArch::from_target(&target).expect(format!("Unsupported target: {}", target).as_str());
+
+    let linker_script = format!("src/arch/{}/kernel_{profile}.ld", arch.to_dir_name());
+    println!("cargo::rustc-link-arg=-T{}", linker_script);
+}
+
+enum SupportedArch {
+    Aarch64,
+}
+
+impl SupportedArch {
+    pub fn from_target(target: &str) -> Option<Self> {
+        match target {
+            "aarch64-unknown-none" => Some(SupportedArch::Aarch64),
+            _ => None,
+        }
+    }
+
+    pub fn to_dir_name(&self) -> &str {
+        match self {
+            SupportedArch::Aarch64 => "arm64",
+        }
+    }
+}
