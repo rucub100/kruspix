4a2e2b5245afedd2abaa82a323460269998ddf4d
Ruslan Curbanov <info@ruslan-curbanov.de>
2025-11-12 18:23:56 +0100

    feat(panic): enhance panic handler with detailed output information

--- src/panic_handler.rs
+++ src/panic_handler.rs
@@ -1,6 +1,16 @@
 use core::panic::PanicInfo;
+use crate::kprintln;
 
 #[panic_handler]
 pub fn panic(_info: &PanicInfo) -> ! {
+    kprintln!("\n\n\n\n\n\nPANIC OCCURRED:");
+    if let Some(location) = _info.location() {
+        kprintln!("  File: {}:{}:{}", location.file(), location.line(), location.column());
+    } else {
+        kprintln!("  No location information available.");
+    }
+
+    kprintln!("  Message: {}", _info.message());
+    
     loop {}
 }
